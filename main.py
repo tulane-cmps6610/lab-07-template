b3766a36c6a90763f561b4e5da2ddeefadf79a93
rrmettu <rmettu@tulane.edu>
2024-10-22 14:46:11 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,60 @@
+from collections import defaultdict
+
+def make_undirected_graph(edge_list):
+    """ Makes an undirected graph from a list of edge tuples. """
+    graph = defaultdict(set)
+    for e in edge_list:
+        graph[e[0]].add(e[1])
+        graph[e[1]].add(e[0])
+    return graph
+
+
+def reachable(graph, start_node):
+    """
+    Returns:
+      the set of nodes reachable from start_node
+    """
+    result = set([start_node])
+    frontier = set([start_node])
+    while len(frontier) != 0:
+        ###TODO
+        pass
+    return result
+
+def test_reachable():
+    graph = make_undirected_graph([('A', 'B'), ('B', 'C'), ('C', 'D'), ('D', 'B')])
+    assert sorted(reachable(graph, 'A')) == ['A', 'B', 'C', 'D']
+
+    graph = make_undirected_graph([('A', 'B'), ('B', 'C'), ('C', 'D'), ('D', 'B'), ('E', 'F'), ('F', 'G')])
+    assert sorted(reachable(graph, 'A')) == ['A', 'B', 'C', 'D']
+    assert sorted(reachable(graph, 'E')) == ['E', 'F', 'G']
+
+
+
+
+def connected(graph):
+    ### TODO
+    pass
+
+def test_connected():
+    graph = make_undirected_graph([('A', 'B'), ('B', 'C'), ('C', 'D'), ('D', 'B')])
+    assert connected(graph) == True
+    graph = make_undirected_graph([('A', 'B'), ('B', 'C'), ('C', 'D'), ('D', 'B'), ('E', 'F'), ('F', 'G')])
+    assert connected(graph) == False
+
+
+
+def n_components(graph):
+    """
+    Returns:
+      the number of connected components in an undirected graph
+    """
+    ### TODO
+    pass
+
+def test_n_components():
+    graph = make_undirected_graph([('A', 'B'), ('B', 'C'), ('C', 'D'), ('D', 'B')])
+    assert n_components(graph) == 1
+
+    graph = make_undirected_graph([('A', 'B'), ('B', 'C'), ('C', 'D'), ('D', 'B'), ('E', 'F'), ('F', 'G')])
+    assert n_components(graph) == 2
